77105644a2714a5797c514897cf98fefbfca6d0d
Dheeraj Duggempudi <dheeraj.duggempudi@gmail.com>
2020-01-17 13:42:55 +0530

    'dynamic-image'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+declare const gettimage:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements InMemoryDbService{
+  createDb(reqInfo?: import("angular-in-memory-web-api").RequestInfo): {} | import("rxjs").Observable<{}> | Promise<{}> {
+    throw new Error("Method not implemented.");
+  }
+  selectedFile=null;
   title = 'thaiform';
+  gtevent(event){
+    this.selectedFile=event;
+  }
+  onFileUpload(){
+    var image = document.getElementById('dynamicimg') as HTMLInputElement;
+    image.src = URL.createObjectURL(this.selectedFile.target.files[0]);
+  }
 }
---
+++ src/assets/jsfiles/imagegrab.js
@@ -0,0 +1,5 @@
+function gettimage(event){
+    //var flname=this.document.getElementById('imgfile').Value;event.target.files[0].name
+    window.alert(document.getElementById(memnum));
+    var image = document.getElementById('');
+}
